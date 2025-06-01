bc9e3b328285f10b210c2d6466eb9867a2311548
Herbalplantsmar <marbk2019@gmail.com>
2025-06-01 14:57:08 +0200

    Create plant-data.js

---
+++ plant-data.js
@@ -0,0 +1,16 @@
+const plantDatabase = {
+  pimpernel: {
+    name: "Kleine pimpernel",
+    latin: "Sanguisorba minor",
+    image: "Afbeeldingen/IMG_7433.jpg",
+    recipe: "Heerlijk in een salade met jonge blaadjes en wat citroenolie.",
+    info: "Kleine pimpernel is een eetbare wilde plant die vaak voorkomt op droge graslanden."
+  },
+  hazelaar: {
+    name: "Hazelaar",
+    latin: "Corylus avellana",
+    image: "Afbeeldingen/plant2.jpg",
+    recipe: "Combineer de jonge bladeren met walnoten en honing in een groene smoothie.",
+    info: "De hazelaar is een veelvoorkomende struik in Europa met eetbare noten."
+  },
+
