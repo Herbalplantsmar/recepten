9ca6e8c76254f71d0038f42caed52e13d15957ae
Herbalplantsmar <marbk2019@gmail.com>
2025-06-17 22:13:00 +0200

    Update plant.js

--- plant.js
+++ plant.js
@@ -9,6 +9,17 @@ if (plant) {
   document.getElementById("plant-image").alt = plant.name;
   document.getElementById("plant-recipe").innerText = plant.recipe;
   document.getElementById("plant-info").innerText = plant.info;
+  // Show extra images if available
+if (plant.images && plant.images.length > 0) {
+  const extraImagesContainer = document.getElementById("extra-images");
+
+  plant.images.forEach((imgSrc) => {
+    const img = document.createElement("img");
+    img.src = imgSrc;
+    img.alt = plant.name;
+    extraImagesContainer.appendChild(img);
+  });
+
 } else {
   document.body.innerHTML = "<h1>Plant niet gevonden.</h1><a href='index.html'>← Terug</a>";
 }
